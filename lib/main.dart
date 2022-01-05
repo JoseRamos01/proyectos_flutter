bd1681e673a67215417823f230182d98891ee64f
Joseph <jose.ramos01@globant.com>
2022-01-05 18:33:24 -0300

    Counter created

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  messagePrint() {
-  print('Function Called');
+class MyApp extends StatefulWidget {
+  @override
+  _BusinessCard createState() => _BusinessCard();
+}
+class _BusinessCard extends State<MyApp> {
+int _counter = 0;
+
+void incrementCounter(){
+  setState(() {
+    _counter++;
+  });
 }
 
 @override
@@ -28,7 +36,7 @@ Widget build(BuildContext context) {
             Text(
               'Jose Ramos',
               style: TextStyle(
-                fontSize: 40,
+                fontSize: 42,
                 color: Colors.black87,
               ),
               textAlign: TextAlign.center,
@@ -40,16 +48,24 @@ Widget build(BuildContext context) {
             ),
             textAlign: TextAlign.center,
             ),
-          SizedBox(height: 150),
+          SizedBox(height: 40),
+            Container(
+              child: Padding(padding: EdgeInsets.fromLTRB(25, 10, 25, 25),
+                child: Text('$_counter' + ' personas recomiendan este profesional', style: TextStyle(
+                fontSize: 22,
+                color: Colors.black,
+              )),
+            )),
             Padding(padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
             child: Row(
                 children: <Widget> [
             Padding(padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
                 child: ImageIcon(
-                    AssetImage('assets/images/mail.png'),
+                    AssetImage('assets/images/mail.png',),
+                    size: 23,
                     )),
                   Text('jmramoslemes@gmail.com', style: TextStyle(
-                    fontSize: 24,
+                    fontSize: 20,
                     color: Colors.black,
                   ),
                     textAlign: TextAlign.center,
@@ -62,9 +78,10 @@ Widget build(BuildContext context) {
               Padding(padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
                 child: ImageIcon(
                       AssetImage('assets/images/wpp.png'),
+                      size: 25,
                     )),
                     Text('095880709', style: TextStyle(
-                      fontSize: 24,
+                      fontSize: 20,
                       color: Colors.black,
                     ),
                       textAlign: TextAlign.center,
@@ -77,16 +94,36 @@ Widget build(BuildContext context) {
                     Padding(padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
                     child: ImageIcon(
                       AssetImage('assets/images/linkedin.png'),
+                      size: 26,
                     )
                     ),
                     Text('linkedin.com/jose.ramos', style: TextStyle(
-                      fontSize: 24,
+                      fontSize: 20,
                       color: Colors.black,
                     ),
                       textAlign: TextAlign.center,
                     ),
                   ]),
             ),
+              SizedBox(height: 40,),
+              Column(
+                children: <Widget> [
+                  Center(
+                    child: IconButton(
+                      icon: ImageIcon(AssetImage('assets/images/like.png')
+                    ),
+                      onPressed: () { incrementCounter(); },
+                      iconSize: 50,
+                    ),
+                  ),
+                  Center(
+                    child: Text('Recomendar', style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.black,
+                    ),)
+                  )
+                ],
+              )
           ])
       ))));
 }
